4fa080f09cf9291a72bd32eb4096a7acfd9d28e0
antoniojoacir <antonjoacir@gmail.com>
2025-05-09 18:12:49 -0300

    replace: starter do mini.nvim em vez do alpha

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,31 +1,74 @@
-return { -- Collection of various small independent plugins/modules
+return {
 	"echasnovski/mini.nvim",
 	config = function()
-		-- Better Around/Inside textobjects
-		--
-		-- Examples:
-		--  - va)  - [V]isually select [A]round [)]paren
-		--  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-		--  - ci'  - [C]hange [I]nside [']quote
-		require("mini.ai").setup({ n_lines = 500 })
-
-		-- Add/delete/replace surroundings (brackets, quotes, etc.)
-		--
-		-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-		-- - sd'   - [S]urround [D]elete [']quotes
-		-- - sr)'  - [S]urround [R]eplace [)] [']
+		require("mini.ai").setup()
 		require("mini.surround").setup()
 
-		-- local statusline = require("mini.statusline")
-		-- statusline.setup({ use_icons = vim.g.have_nerd_font })
-		--
-		-- You can configure sections in the statusline by overriding their
-		-- default behavior. For example, here we set the section for
-		-- cursor location to LINE:COLUMN
-		-- ---@diagnostic disable-next-line: duplicate-set-field
-		-- statusline.section_location = function()
-		-- 	return "%2l:%-2v"
-		-- end
-		-- woid
+		local statusline = require("mini.statusline")
+		statusline.setup({ use_icons = vim.g.have_nerd_font })
+
+		---@diagnostic disable-next-line: duplicate-set-field
+		statusline.section_location = function()
+			return "%2l:%-2v"
+		end
+
+		local starter = require("mini.starter")
+
+		starter.setup({
+			evaluate_single = false,
+			header = table.concat({
+				[[
+
+			 	   ,-.       _,---._ __  / \
+				  /  )    .-'       `./ /   \
+				 (  (   ,'            `/    /|
+				  \  `-"             \'\   / |
+				   `.              ,  \ \ /  |
+				    /`.          ,'-`----Y   |
+				   (            ;        |   '
+				   |  ,-.    ,-'         |  /
+				   |  | (   |            | /
+				   )  |  \  `.___________|/
+				   `--'   `--'
+
+				]],
+			}, "\n"),
+			footer = os.date("%B %d, %I:%M %p"),
+			items = {
+				{
+					name = "  Bookmarked Files",
+					action = "lua MiniExtra.pickers.visit_paths { filter = 'todo' }",
+					section = " Actions ",
+				},
+				{
+					name = "  Lazy Update",
+					action = ":Lazy update",
+					section = " Actions ",
+				},
+				{
+					name = "  Open Blank File",
+					action = ":enew",
+					section = " Actions ",
+				},
+				{
+					name = "  Find Files",
+					action = "lua MiniPick.builtin.files()",
+					section = " Actions ",
+				},
+				{
+					name = "  Recent Files",
+					action = "lua MiniExtra.pickers.oldfiles()",
+					section = " Actions ",
+				},
+				{
+					name = "  Quit",
+					action = ":q!",
+					section = " Actions ",
+				},
+			},
+			content_hooks = {
+				starter.gen_hook.aligning("center", "center"),
+			},
+		})
 	end,
 }
