ee1ce0caed713649bf7ca7995df9aff667a54161
antoniojoacir <antonjoacir@gmail.com>
2025-05-09 16:46:22 -0300

    mudando arquivos de localização e alterando algumas coisas

--- init.lua
+++ init.lua
@@ -5,19 +5,41 @@ vim.g.maplocalleader = " "
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
-require("custom.vimopts")
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
 
--- [[ Basic Keymaps ]]
-require("custom.vimkeymaps")
+vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
-require("custom.lazy")
+require("lazy").setup({
+	spec = {
+		"tpope/vim-sleuth",
+		{ "Bilal2453/luvit-meta", lazy = true },
+		{ import = "custom.plugins" },
+	},
+	defaults = {
+		version = false,
+	},
+	change_detection = {
+		enabled = true,
+		notify = false, -- get a notification when changes are found
+	},
+})
 
 -- Theme
 vim.cmd.colorscheme("base16-black-metal-bathory")
-
--- vim.cmd.colorscheme("tokyonight")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
