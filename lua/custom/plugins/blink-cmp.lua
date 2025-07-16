2aa518c756a1b050b62bc11e8cedfbc839212967
antoniojoacir <antonjoacir@gmail.com>
2025-07-16 19:36:44 -0300

    config blink and twilight

--- lua/custom/plugins/blink-cmp.lua
+++ lua/custom/plugins/blink-cmp.lua
@@ -1,18 +1,30 @@
 return {
 	{
 		"saghen/blink.cmp",
-		-- event = "VimEnter",
+		event = "VimEnter",
 		version = "1.*",
 		dependencies = {
 			"L3MON4D3/LuaSnip",
 			version = "v2.*",
+			dependencies = {
+				"folke/lazydev.nvim",
+			},
 		},
 		opts = {
+			keymap = {
+				preset = "default",
+			},
 			signature = { enabled = true },
 			appearance = { nerd_font_variant = "mono" },
 			snippets = { preset = "luasnip" },
 			sources = {
-				default = { "lsp", "path", "snippets", "buffer" },
+				default = { "lsp", "path", "snippets", "lazydev" },
+				providers = {
+					lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+				},
+			},
+			fuzzy = {
+				implementation = "lua",
 			},
 			completion = {
 				documentation = { auto_show = true },
